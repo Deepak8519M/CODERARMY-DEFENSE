1acf6f69821efb5898fc9dffb22f2e2dd839aad2
Deepak Mallareddy <deepak8519m@gmail.com>
2025-09-25 19:00:20 +0530

    code added

--- JAVASCRIPT/DAY17/index.js
+++ JAVASCRIPT/DAY17/index.js
@@ -1,12 +1,41 @@
-// Variable Creation
+// data Types
 
-let name = "Rohit";
-let age = 20;
-age = 30;
+// primitive data type
+// number,string,boolean,undefined,null,bigInt,symbol
 
-const account = 1234;
-account = 4567;
+let a = 10;
+let b = 20.35;
+console.log(b);
+console.log(a);
 
-console.log("Hello " + name + " age is " + age);
+let c = "Strike is Coming";
 
-console.log(account);
+let d = "Rohit";
+
+console.log(c + " " + d);
+
+// non Primitve Data Type
+//array,obj,function,
+
+let f = false;
+
+console.log(f);
+
+let user;
+
+console.log(user);
+
+const p = 2;
+console.log(p);
+
+let num = 1193280930983083028390898989098908908938;
+console.log(num);
+
+let weather = null;
+console.log(weather);
+
+const id1 = Symbol("id");
+const id2 = Symbol("id");
+
+console.log(id2 == id1);
+console.log(id1);
--- JAVASCRIPT/DAY17/readme.md
+++ JAVASCRIPT/DAY17/readme.md
@@ -1,1 +1,30 @@
 https://www.notion.so/DAY17-25th-Sept-279beb1a72a480f2a25adcf219926b69?source=copy_link
+
+// Variable Creation
+
+// let name = "Rohit";
+// let age = 20;
+// age = 30;
+
+// const account = 1234;
+// account = 4567;
+
+// console.log("Hello " + name + " age is " + age);
+
+// console.log(account);
+
+// var a = 10;
+
+// var b = (a += 20);
+
+// console.log(b);
+// console.log(a);
+
+// if (true) {
+// var a = 10;
+// }
+
+// var a = 20;
+// console.log(a);
+
+// fj
