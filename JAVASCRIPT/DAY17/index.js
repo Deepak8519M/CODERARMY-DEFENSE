f4edf4a60deb2cb25719c6b46b6d68eab1908ea9
Deepak Mallareddy <deepak8519m@gmail.com>
2025-09-25 19:10:56 +0530

    code added

--- JAVASCRIPT/DAY17/index.js
+++ JAVASCRIPT/DAY17/index.js
@@ -1,47 +1,81 @@
-// data Types
+// // // data Types
 
-// primitive data type
-// number,string,boolean,undefined,null,bigInt,symbol
+// // // primitive data type
+// // // number,string,boolean,undefined,null,bigInt,symbol
+
+// // // let a = 10;
+// // // let b = 20.35;
+// // // console.log(b);
+// // // console.log(a);
+
+// // // let c = "Strike is Coming";
+
+// // // let d = "Rohit";
+
+// // // console.log(c + " " + d);
+
+// // // // non Primitve Data Type
+// // // //array,obj,function,
+
+// // // let f = false;
+
+// // // console.log(f);
+
+// // // let user;
+
+// // // console.log(user);
+
+// // // const p = 2;
+// // // console.log(p);
+
+// let num = 1193280930983083028390898989098908908938n;
+// console.log(typeof num);
+
+// // // console.log(num);
+
+// // // let weather = null;
+// // // console.log(weather);
+
+// // // const id1 = Symbol("id");
+// const id2 = Symbol("id");
+// console.log(typeof id2);
+
+// // // console.log(id2 == id1);
+// // // console.log(id1);
+
+// // // let arr = [10, 20, 30, 40, "Rohit", true, undefined, null];
+
+// // // console.log(arr);
+
+// // const obj = {
+// //   name: "Rohit Negi",
+// //   accountNo: 12312,
+// //   age: 20,
+// //   category: "General",
+// // };
+
+// // console.log(obj);
+
+// // function add() {
+// //   return "Hello";
+// // }
+
+// // let b = add();
+// // console.log(b);
+// // console.log(typeof b);
+
+// // let s = function add() {
+// //   return "Hello";
+// // };
+// // console.log(s());
 
 // let a = 10;
-// let b = 20.35;
-// console.log(b);
-// console.log(a);
 
-// let c = "Strike is Coming";
+// let b = 2.45;
 
-// let d = "Rohit";
-
-// console.log(c + " " + d);
-
-// // non Primitve Data Type
-// //array,obj,function,
-
-// let f = false;
-
-// console.log(f);
-
-// let user;
-
-// console.log(user);
-
-// const p = 2;
-// console.log(p);
-
-// let num = 1193280930983083028390898989098908908938;
-// console.log(num);
+// console.log(typeof a);
+// console.log(typeof b);
 
 // let weather = null;
-// console.log(weather);
 
-// const id1 = Symbol("id");
-// const id2 = Symbol("id");
-
-// console.log(id2 == id1);
-// console.log(id1);
-
-// let arr = [10, 20, 30, 40, "Rohit", true, undefined, null];
-
-// console.log(arr);
-
-
+// console.log(typeof weather);
