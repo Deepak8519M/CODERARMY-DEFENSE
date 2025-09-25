516c7a8f0ce0f07a210ee72cb447651f02f66af7
Deepak Mallareddy <deepak8519m@gmail.com>
2025-09-25 18:32:21 +0530

    code added

---
+++ JAVASCRIPT/DAY17/index.js
@@ -0,0 +1,12 @@
+// Variable Creation
+
+let name = "Rohit";
+let age = 20;
+age = 30;
+
+const account = 1234;
+account = 4567;
+
+console.log("Hello " + name + " age is " + age);
+
+console.log(account);
---
+++ JAVASCRIPT/DAY17/readme.md
@@ -0,0 +1,1 @@
+https://www.notion.so/DAY17-25th-Sept-279beb1a72a480f2a25adcf219926b69?source=copy_link
