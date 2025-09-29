c9e4120c657070bc6a29df5b2ea0500b04108b1c
Deepak Mallareddy <deepak8519m@gmail.com>
2025-09-29 18:16:57 +0530

    code added

---
+++ JAVASCRIPT/DAY19/index.js
@@ -0,0 +1,26 @@
+// console.log(2 + 5);
+// console.log(2 - 5);
+// console.log(2 * 5);
+// console.log(5 % 2);
+
+// console.log(2 ** 4);
+
+// let x = 20;
+
+// let y = 10;
+
+// x = x - y;
+
+// x += x * 10;
+
+// console.log(x);
+
+// let z = "10";
+// let a = Number(z);
+// console.log(typeof a);
+// console.log(typeof z);
+
+// console.log(z === 10);
+
+
+// type check hota hai dono shoud be of same type 
