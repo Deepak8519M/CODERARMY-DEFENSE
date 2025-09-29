8070cbcb57a9c9c57dfc8633dd97ba3ad44431d1
Deepak Mallareddy <deepak8519m@gmail.com>
2025-09-29 18:20:57 +0530

    code added

--- JAVASCRIPT/DAY19/index.js
+++ JAVASCRIPT/DAY19/index.js
@@ -24,10 +24,17 @@
 
 // type check hota hai dono shoud be of same type
 
-let a = 121;
-let b = Number(a);
-console.log(b);
+// let a = 121;
+// let b = Number(a);
+// console.log(b);
 
-let c = String(b);
-console.log(c);
-console.log(typeof c)
+// let c = String(b);
+// console.log(c);
+// console.log(typeof c)
+
+let a = true;
+let b = false;
+console.log(Number(a));
+console.log(Number(b));
+console.log(Number(null));
+console.log(Number(undefined));
