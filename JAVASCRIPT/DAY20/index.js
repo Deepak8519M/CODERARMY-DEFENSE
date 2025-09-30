7ffde17904386cf9c219a890653e801b34faad7d
Deepak Mallareddy <deepak8519m@gmail.com>
2025-09-30 19:07:35 +0530

    assets added

--- JAVASCRIPT/DAY20/index.js
+++ JAVASCRIPT/DAY20/index.js
@@ -38,6 +38,8 @@
 // for (let i = 0; i <= 30; i++) {
 //   console.log(Math.floor(Math.random() * 6) + 1);
 // }
-for (let i = 0; i < 20; i++) {
-  console.log(Math.floor(Math.random() * 11) + 15);
-}
+// for (let i = 0; i < 20; i++) {
+//   console.log(Math.floor(Math.random() * 11) + 15);
+// }
+
+console.log(Math.floor(Math.random() * (9999 - 999 + 1)) + 1000);
