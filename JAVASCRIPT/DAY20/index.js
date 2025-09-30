bc47810d565a0baff71a0c3810c87c8d1ee2ffbf
Deepak Mallareddy <deepak8519m@gmail.com>
2025-09-30 18:17:19 +0530

    code added

--- JAVASCRIPT/DAY20/index.js
+++ JAVASCRIPT/DAY20/index.js
@@ -1,5 +1,11 @@
 let a = 10;
 
 let b = 345.7829;
+let c = b.toFixed(1);
+console.log(b);
+console.log(c);
+console.log(typeof c);
+// --> returns string
 
-console.log(b.toFixed(2));
+console.log(b.toPrecision(10));
+console.log(typeof b);
