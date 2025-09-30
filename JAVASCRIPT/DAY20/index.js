a702511899101709ac08372388a0af2271ffa287
Deepak Mallareddy <deepak8519m@gmail.com>
2025-09-30 18:23:07 +0530

    code added

--- JAVASCRIPT/DAY20/index.js
+++ JAVASCRIPT/DAY20/index.js
@@ -1,11 +1,25 @@
-let a = 10;
+// let a = 10;
 
-let b = 345.7829;
-let c = b.toFixed(1);
-console.log(b);
-console.log(c);
-console.log(typeof c);
-// --> returns string
+// let b = 345.7829;
+// let c = b.toFixed(1);
+// console.log(b);
+// console.log(c);
+// console.log(typeof c);
+// // --> returns string
 
-console.log(b.toPrecision(10));
-console.log(typeof b);
+// console.log(b.toPrecision(10));
+// console.log(typeof b);
+
+// let a = new Number(20);
+// // let b = 20;
+// let b = new Number(20);
+
+// console.log(typeof a);
+// console.log(typeof b);
+// console.log(b);
+
+let obj1 = {
+  name: "Rohit",
+};
+let obj2 = obj1;
+console.log(obj1 == obj2);
