92406644aa37003535b73fc0d80bfd8e6d8dd110
Deepak Mallareddy <deepak8519m@gmail.com>
2025-10-02 00:28:51 +0530

    slice method added

--- JAVASCRIPT/DAY21/index.js
+++ JAVASCRIPT/DAY21/index.js
@@ -7,21 +7,23 @@
 
 // const str3 = `Strike is Coming Soon on ${day}`;
 
-// console.log(str1, str2, str3);
+// // console.log(str1, str2, str3);
+
+// console.log(str.length);
+// console.log(str[0]);
+
+// // for (i = 0; i < str.length; i++) {
+// //   console.log(str[i]);
+// // }
+// console.log(str.toUpperCase());
+// console.log(str);
+
+// str[0] = "A";
+
+// console.log(str[0]);
+// console.log(str.includes("Am"));
+// console.log(str.lastIndexOf("Hel"));
 
 const str = `Hello Coder Army Hello`;
-
-console.log(str.length);
-console.log(str[0]);
-
-// for (i = 0; i < str.length; i++) {
-//   console.log(str[i]);
-// }
-console.log(str.toUpperCase());
-console.log(str);
-
-str[0] = "A";
-
-console.log(str[0]);
-console.log(str.includes("C"));
-console.log(str.lastIndexOf("Hel"));
+console.log(str.slice(0, 2));
+console.log(str.slice(3));
