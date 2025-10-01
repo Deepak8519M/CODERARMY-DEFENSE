fdcf462d78e13e0bfc38b98f39a20e2be217170e
Deepak Mallareddy <deepak8519m@gmail.com>
2025-10-02 00:23:04 +0530

    string methods arre eing added

--- JAVASCRIPT/DAY21/index.js
+++ JAVASCRIPT/DAY21/index.js
@@ -1,10 +1,25 @@
-const str1 = "Rohit";
+// const str1 = "Rohit";
 
-const str2 = "Rohit Negi";
-const day = 18;
+// const str2 = "Rohit Negi";
+// const day = 18;
 
-// console.log(str1 && str2);
+// // console.log(str1 && str2);
 
-const str3 = `Strike is Coming Soon on ${day}`;
+// const str3 = `Strike is Coming Soon on ${day}`;
 
-console.log(str1, str2, str3);
+// console.log(str1, str2, str3);
+
+const str = `Hello Coder Army `;
+
+console.log(str.length);
+console.log(str[0]);
+
+// for (i = 0; i < str.length; i++) {
+//   console.log(str[i]);
+// }
+console.log(str.toUpperCase());
+console.log(str);
+
+str[0] = "A";
+
+console.log(str[0]);
