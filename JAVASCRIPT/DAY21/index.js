750a9bcb110c476e22cb4801e934fddafc4764fe
Deepak Mallareddy <deepak8519m@gmail.com>
2025-10-02 00:19:36 +0530

    dynamic templated added

--- JAVASCRIPT/DAY21/index.js
+++ JAVASCRIPT/DAY21/index.js
@@ -1,9 +1,10 @@
 const str1 = "Rohit";
 
 const str2 = "Rohit Negi";
+const day = 18;
 
 // console.log(str1 && str2);
 
-const str3 = `Strike is Coming Soon`;
+const str3 = `Strike is Coming Soon on ${day}`;
 
 console.log(str1, str2, str3);
