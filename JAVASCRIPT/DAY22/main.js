07d388ed2a05cee85efdec53f6529c73009a48a3
Deepak Mallareddy <deepak8519m@gmail.com>
2025-10-03 22:11:03 +0530

    for of loop added

--- JAVASCRIPT/DAY22/main.js
+++ JAVASCRIPT/DAY22/main.js
@@ -1,7 +1,11 @@
 //
 
+// for (let i = 0; i < arr.length; i++) {
+//   console.log(arr[i]);
+// }
+
 let arr = [10, 20, 50, 60, 90];
 
-for (let i = 0; i < arr.length; i++) {
-  console.log(arr[i]);
+for (let a in arr) {
+  console.log(a);
 }
