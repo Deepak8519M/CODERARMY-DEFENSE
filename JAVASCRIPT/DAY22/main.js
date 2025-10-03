44575061b3c250a05a879b6858e1daf492ef664e
Deepak Mallareddy <deepak8519m@gmail.com>
2025-10-03 22:42:32 +0530

    why array is obj explained and code pushed

--- JAVASCRIPT/DAY22/main.js
+++ JAVASCRIPT/DAY22/main.js
@@ -38,9 +38,11 @@
 
 const a = [10, 20, 300, [10, 20, 30, [40, 50, 60]], 40, 4, 100];
 
+a.name = "John";
+
 // a.sort((a, b) => a - b);
 // 52:38 sec rule and intution behind the sort function
 
 // console.log(a[3][3][2]);
 
-console.log(a.flat(Infinity));
+console.log(a);
